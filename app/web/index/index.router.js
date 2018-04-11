044264b7fc96d5fe53303458a888b6c9d9135e38
James Michael Adoremos <adoremos162@gmail.com>
2018-04-11 17:10:14 +0800

    Added new files for index

---
+++ app/web/index/index.router.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+// we need to start a routing instance
+let router = express.Router();
+
+// we define the route for GET requests
+let getRoute = function(req, res) {
+  res.sendFile(path.join(__dirname, './index.view.html'));
+  res.flush();
+};
+
+/* route: / */
+router.route('/')
+  .get(getRoute);
+
+module.exports = router;
